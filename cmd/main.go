7e131b2d180394191ca5a0a844da1d67858dec30
Chris Wiegman <chris@chriswiegman.com>
2019-10-10 09:50:26 -0400

    add subcommand to remove entire section

--- cmd/main.go
+++ cmd/main.go
@@ -22,6 +22,7 @@ Usage:
   goodhosts add <ip> <host>...
   goodhosts (rm|remove) <ip> <host>...
   goodhosts list [--all]
+  goodhosts removesection <section>
   goodhosts -h | --help
   goodhosts --version
 
@@ -30,7 +31,7 @@ Options:
   -h --help     Show this screen.
   --version     Show the version.`
 
-	args, _ := docopt.Parse(usage, nil, true, "Goodhosts 3.1.1", false)
+	args, _ := docopt.Parse(usage, nil, true, "Goodhosts 3.2.0", false)
 
 	hosts, err := goodhosts.NewHosts("")
 	check(err)
@@ -109,6 +110,30 @@ Options:
 		}
 
 		err = hosts.Remove(ip, hostEntries...)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", err.Error()))
+			os.Exit(2)
+		}
+
+		err = hosts.Flush()
+		check(err)
+
+		return
+	}
+
+	if args["removesection"].(bool) {
+
+		section := args["<section>"].(string)
+
+		hosts, err := goodhosts.NewHosts(section)
+		check(err)
+
+		if !hosts.IsWritable() {
+			fmt.Fprintln(os.Stderr, "Host file not writable. Try running with elevated privileges.")
+			os.Exit(1)
+		}
+
+		err = hosts.RemoveSection()
 		if err != nil {
 			fmt.Fprintf(os.Stderr, fmt.Sprintf("%s\n", err.Error()))
 			os.Exit(2)
